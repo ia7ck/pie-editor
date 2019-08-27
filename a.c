353a67f883d065a995ea57b20a30976bc3c24eb2
ia7ck <23146842+ia7ck@users.noreply.github.com>
2019-08-27 20:13:56 +0900

    first commit

---
+++ a.c
@@ -0,0 +1,22 @@
+#include "ox_toolkit.h"
+#include <string.h>
+
+// important!
+extern OXFILE *ox_start(char *host, char *prog1, char *prog2);
+OXFILE *start(void) { return ox_start("localhost", "ox", "ox_asir"); }
+void execute_string(OXFILE *server, char *str) {
+  ox_execute_string(server, str);
+}
+char *pop_string(OXFILE *server) { return ox_popString(server); }
+void shutdown(OXFILE *server) { ox_shutdown(server); }
+
+int main(void) {
+
+  OXFILE *server = start();
+  execute_string(server, "1000000000 + 98765;");
+  char *result = pop_string(server);
+  printf("%s\n", result);
+  shutdown(server);
+
+  return 0;
+}
---
+++ b.c
@@ -0,0 +1,18 @@
+#include "ox_toolkit.h"
+#include <stdio.h>
+
+extern OXFILE *start(void);
+extern void execute_string(OXFILE *, char *);
+extern char *pop_string(OXFILE *);
+extern void shutdown(OXFILE *);
+
+int main(void) {
+
+  OXFILE *server = start();
+  execute_string(server, "1000000000 + 98765;");
+  char *result = pop_string(server);
+  printf("%s\n", result);
+  shutdown(server);
+
+  return 0;
+}
