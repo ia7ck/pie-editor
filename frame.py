10f362634ad699eba7c1c68451bde5fbe11c032b
ia7ck <23146842+ia7ck@users.noreply.github.com>
2019-08-29 09:11:32 +0900

    create editor frame

---
+++ frame.py
@@ -0,0 +1,19 @@
+import kivy.app
+import kivy.core.window
+import kivy.uix.button
+import kivy.uix.boxlayout
+
+kivy.core.window.Window.clearcolor = (1, 1, 1, 1)
+
+
+class Editor(kivy.app.App):
+    def build(self):
+        self.layout = kivy.uix.boxlayout.BoxLayout(orientation="vertical")
+        self.layout.add_widget(kivy.uix.button.Button(text="hello"))
+        self.layout.add_widget(kivy.uix.button.Button(text="world"))
+        return self.layout
+
+
+if __name__ == "__main__":
+    editor = Editor()
+    editor.run()
