5e7b788db115dadd3eb0453d5736de968e939715
ia7ck <23146842+ia7ck@users.noreply.github.com>
2019-09-04 21:37:07 +0900

    fix scanner for multi error

--- scanner.py
+++ scanner.py
@@ -2,23 +2,36 @@ import re
 
 
 # return -1 if not found
+# return first line number (>=1) error occurred
 def get_error_line(text):
     if not ("error" in text):
         return -1
-    sp = text.split("asir_where")
-    if len(sp) < 2:
+    m = re.search(r"asir_where,(.*)\]\]\)", text)
+    if m == None:  # match しなかった
         return -1
-    match = re.search(r"(\d+)", sp[1])
-    if match:
-        return int(match.group(1))
-    else:
+    errors = m.group(1)
+    error_line_numbers = [
+        n for n in list(map(int, re.findall(r"\d+", errors))) if n >= 1
+    ]
+    if len(error_line_numbers) == 0:
         return -1
+    return min(error_line_numbers)
 
+
+"""
+def g() {
+    h();
+}
+def f() { 
+    g();
+} 
+f();
+"""
 
 if __name__ == "__main__":
     assert (
         get_error_line(
-            "error([1,4294967295,syntax error,[asir_where,[[toplevel,13]]]])"
+            "error([7,4294967295,evalf : h undefined,[asir_where,[[toplevel,7],[string,f,5],[string,g,2],[,end,0]]]])"
         )
-        == 13
+        == 2
     )
