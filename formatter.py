8ea1b096eee431d54338245ddb90ecde6f345014
ia7ck <23146842+ia7ck@users.noreply.github.com>
2019-09-25 12:59:04 +0900

    add helper method

--- formatter.py
+++ formatter.py
@@ -3,7 +3,7 @@ import textwrap
 
 def split_tokens(s):
     i, n = 0, len(s)
-    s += " "  # s[n] == " "
+    s += "$"  # s[n] == "$"
     tokens, delims = [], set(list("+-*/%^=,:;$(){}!?<>[]&|"))
     while i < n:
         if s[i] == '"':  # 文字列の中は token に分割しない
@@ -16,12 +16,12 @@ def split_tokens(s):
             tokens.append('"' + string + '"')
             i += 1
         elif s[i] == "#":  # #if FLAG, #endif, ...
-            pre = ""
+            directive = ""
             i += 1
             while i < n and s[i] != "\n":
-                pre += s[i]
+                directive += s[i]
                 i += 1
-            tokens.extend(["#", pre])
+            tokens.extend(["#", directive])
             i += 1
         elif s[i : i + 2] == "//":
             comment = ""
@@ -29,7 +29,7 @@ def split_tokens(s):
             while i < n and s[i] != "\n":
                 comment += s[i]
                 i += 1
-            tokens.extend(["//", comment.lstrip()])  # 先頭の空白を取り除く
+            tokens.extend(["//", comment])
             i += 1
         elif s[i : i + 2] == "/*":
             comment = ""
@@ -38,7 +38,7 @@ def split_tokens(s):
                 comment += s[i]
                 i += 1
             assert s[i : i + 2] == "*/", "got: '{}', want: '*/'".format(s[i : i + 2])
-            tokens.extend(["/*", comment.strip("\n"), "*/"])  # 先頭末尾に改行があったら取り除く
+            tokens.extend(["/*", comment.strip("\n"), "*/"])  # 先頭末尾の改行を取り除く
             i += 2
         else:
             c = s[i]
@@ -59,15 +59,15 @@ def split_tokens(s):
                 tokens.append(t)
                 i += len(t)
             elif c in delims:
-                tokens.append(c)
-                i += 1
+                if c == ";" and s[i + 1] == "\n":
+                    tokens.append(";" + "\n")
+                    i += 2
+                else:
+                    tokens.append(c)
+                    i += 1
             else:  # @pi, Abc_123, return, def
                 idt = ""
-                while (
-                    i < n
-                    and (not s[i] in {'"', " ", "\n", "\t"})
-                    and (not s[i] in delims)
-                ):
+                while i < n and (not s[i] in (delims | {'"', " ", "\n", "\t"})):
                     idt += s[i]
                     i += 1
                 tokens.append(idt)
@@ -76,93 +76,153 @@ def split_tokens(s):
     return tokens
 
 
-def should_append_space(text):
-    return len(text) >= 1 and (not text[-1] in {" ", "(", "["})
+class Formatter:
+    def __init__(
+        self,
+        operators=set(
+            "+ - * / % ^ = ? : < > += -= *= %= ^= == <= >= != && | ||".split(" ")
+        ),
+        keywords=set(
+            "def module function global local localf if else return static extern for while".split(
+                " "
+            )
+        ),
+    ):
+        self.operators = operators
+        self.keywords = keywords
 
+        self.text = ""
+        self.current_line = ""
+        self.output_lines = []
+        self.depth = 0
 
-def indent_text(text, depth):
-    return " " * (depth * 4) + text
-
-
-def format_code(input_text):
-    tokens = split_tokens(input_text.strip())
-    tokens.append(" ")
-    output_lines, ln = [], ""
-    depth = 0  # indent の深さ
-    lpar, lpar_at_for, inside_for = 0, -1, False
-    ops = set("+ - * / % ^ = ? : < > += -= *= %= ^= == <= >= != && | ||".split(" "))
-    keywords = set(
-        "def module function global local localf if else return static extern for while".split(
-            " "
+    def should_append_space(self):
+        return len(self.current_line) >= 1 and (
+            not self.current_line[-1] in {" ", "(", "["}
         )
-    )
-    i = 0
-    while i + 1 < len(tokens):
-        t = tokens[i]
-        if t == "$":
-            output_lines.append(indent_text(ln + "$", depth))
-            ln = ""
-        elif t == "#":
-            assert ln == ""
-            output_lines.append("#" + tokens[i + 1])
-            i += 1
-        elif t == "//":
-            if len(ln) >= 1 and ln[-1] != " ":
-                ln += " "
-            output_lines.append(indent_text(ln + t + " " + tokens[i + 1], depth))
-            i += 1
-            ln = ""
-        elif t in {"/*", "*/"}:
-            if len(ln) >= 1:
-                output_lines.append(indent_text(ln, depth))
-                ln = ""
-            output_lines.append(indent_text(t, depth))
-        elif t == ";":
-            if inside_for:  # for (I = 0; I < 10; I++)
-                ln += ";" + " "
+
+    def append_singleline_comment(self, begin, comment):
+        assert comment.count("\n") == 0
+        if len(self.current_line) >= 1 and self.current_line[-1] != " ":
+            self.append_token(" ")
+        self.append_token(begin + " " + comment.strip())
+        if begin == "/*":
+            self.append_token(" " + "*/")
+        self.append_current_line()
+
+    def append_multiline_comment(self, comment):
+        if len(self.current_line) >= 1:
+            self.append_current_line()
+        self.append_token("/*")
+        self.append_current_line()
+        self.append_token(comment)
+        self.append_current_line()
+        self.append_token("*/")
+        self.append_current_line()
+
+    def append_token(self, token):
+        self.current_line += token
+
+    def append_current_line(self):
+        assert len(self.current_line) >= 1
+        self.output_lines.append((" " * (self.depth * 4)) + self.current_line)
+        self.current_line = ""
+
+    def generate_output(self, input_text):
+        self.text = input_text
+        tokens = split_tokens(self.text)
+        tokens.append("$")
+        self.current_line, self.output_lines, self.depth = "", [], 0
+        lpar, lpar_at_for, inside_for = 0, -1, False
+        i = 0
+        while i + 1 < len(tokens):
+            t = tokens[i]
+            if t == "$":
+                self.append_token("$")
+                self.append_current_line()
+            elif t == "#":
+                assert self.current_line == ""
+                self.append_token("#" + tokens[i + 1])
+                self.append_current_line()
+                i += 1
+            elif t == "//":
+                self.append_token("//" + " " + tokens[i + 1].strip())
+                self.append_current_line()
+                i += 1
+            elif t == "/*":
+                assert tokens[i + 2] == "*/", "got: {}, want: */".format(tokens[i + 2])
+                comment = tokens[i + 1]
+                if comment.count("\n") >= 1:  # 複数行コメント
+                    self.append_multiline_comment(comment)
+                else:
+                    if len(self.current_line) >= 1 and self.current_line[-1] != " ":
+                        self.append_token(" ")
+                    self.append_token("/*" + " " + comment.strip() + " " + "*/")
+                i += 2
+            elif t.rstrip() == ";":  # t == ";" or t == ";\n"
+                if inside_for:  # for (I = 0; I < 10; I++)
+                    self.append_token(";" + " ")
+                else:
+                    if tokens[i + 1] in {"//", "/*"}:  # ; // ... or ; /* ... */
+                        self.append_token(";")
+                        begin = tokens[i + 1]
+                        body = tokens[i + 2]
+                        if begin == "/*":
+                            if body.count("\n") >= 1:  # 複数行コメント
+                                self.append_multiline_comment(body)
+                            else:
+                                if t[-1] == "\n":
+                                    self.append_current_line()
+                                self.append_singleline_comment(begin, body)
+                            i += 3
+                        else:  # //
+                            if t[-1] == "\n":
+                                self.append_current_line()
+                            self.append_singleline_comment(begin, body)
+                            i += 2
+                    else:
+                        self.append_token(";")
+                        self.append_current_line()
+            elif t in self.operators:
+                if self.should_append_space():  # (, [, などの直後だと False
+                    self.append_token(" ")
+                self.append_token(t + " ")
+            elif t == "{":
+                self.append_token("{")
+                self.append_current_line()
+                self.depth += 1
+            elif t == "}":
+                self.depth -= 1
+                if tokens[i + 1] == "else":  # } else
+                    self.append_token("}" + " ")
+                else:
+                    self.append_token("}")
+                    self.append_current_line()
+            elif t == ",":
+                self.append_token("," + " ")
+            elif t == ")":
+                if tokens[i + 1].rstrip() in {",", ";", "[", "]", ")"}:
+                    self.append_token(")")
+                else:  # () { ... }, () return
+                    self.append_token(")" + " ")
+                lpar -= 1
+                if lpar == lpar_at_for:
+                    inside_for = False
+            elif t in self.keywords:
+                self.append_token(t + " ")
+                if t == "for":
+                    inside_for = True
+                    lpar_at_for = lpar
             else:
-                output_lines.append(indent_text(ln + ";", depth))
-                ln = ""
-        elif t in ops:
-            if should_append_space(ln):  # (, [, などの直後だと False
-                ln += " "
-            ln += t + " "
-        elif t == "{":
-            output_lines.append(indent_text(ln + "{", depth))
-            ln = ""
-            depth += 1
-        elif t == "}":
-            depth -= 1
-            if tokens[i + 1] == "else":  # } else
-                ln += "}" + " "
-            else:
-                output_lines.append(indent_text(ln + "}", depth))
-                ln = ""
-        elif t == ",":
-            ln += "," + " "
-        elif t == ")":
-            if tokens[i + 1] in {",", ";", "[", "]", ")"}:
-                ln += ")"
-            else:  # () { ... }, () return
-                ln += ")" + " "
-            lpar -= 1
-            if lpar == lpar_at_for:
-                inside_for = False
-        elif t in keywords:
-            ln += t + " "
-            if t == "for":
-                inside_for = True
-                lpar_at_for = lpar
-        else:
-            ln += t
-            if t == "(":
-                lpar += 1
-        i += 1
-    assert depth == 0, "Missing right brace: '}'"
-    assert lpar == 0, "Missing right parenthesis: ')'"
-    if len(ln) >= 1:
-        output_lines.append(indent_text(ln, depth))
-    return "\n".join(output_lines)
+                self.append_token(t)
+                if t == "(":
+                    lpar += 1
+            i += 1
+        assert self.depth == 0, "Missing right brace: '}'"
+        assert lpar == 0, "Missing right parenthesis: ')'"
+        if len(self.current_line) >= 1:
+            self.append_current_line()
+        return "\n".join(self.output_lines)
 
 
 def test_format_code():
@@ -197,37 +257,41 @@ def test_format_code():
         ),
         (
             """
-            A =1 +/*comment  comment*/- 3 ;
+            A =1 +/*   comment  comment*/- 3/*c*//5;/* cm*/
+            A*=A;
             /*   multi
             line
               comment*/
-            /*
+            A=2^3;/*
               a
                 b
             */
             A=-1;
             //   single line
             A =123;//single line2
+            if (1) {//comment
+            }//   comment
             """,
             """
-            A = 1 + 
-            /*
-            comment  comment
-            */
-            - 3;
+            A = 1 + /* comment  comment */ - 3 /* c */ / 5; /* cm */
+            A *= A;
             /*
                multi
             line
               comment
             */
+            A = 2 ^ 3;
             /*
               a
                 b
             */
             A = - 1;
             // single line
-            A = 123;
-            // single line2
+            A = 123; // single line2
+            if (1) {
+                // comment
+            }
+            // comment
             """,
         ),
         (
@@ -320,14 +384,19 @@ def test_format_code():
             """,
         ),
     ]
+    f = Formatter()
     for input_text, output_text in [
         (textwrap.dedent(i).strip(), textwrap.dedent(o).strip()) for i, o in testcases
     ]:
-        formatted = format_code(input_text)
+        formatted = f.generate_output(input_text)
         assert formatted == output_text, "\ngot:\n{}\nwant:\n{}".format(
             formatted, output_text
         )
-        assert formatted == format_code(formatted)
+        assert formatted == f.generate_output(
+            formatted
+        ), "\nformat once:\n{}\nformat twice:\n{}".format(
+            formatted, f.generate_output(formatted)
+        )
 
 
 if __name__ == "__main__":
