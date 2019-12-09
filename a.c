e7dfbbe04c879ae764d29d88f9f47c55b42be488
ia7ck <23146842+ia7ck@users.noreply.github.com>
2019-12-09 18:18:19 +0900

    add sleep

--- a.c
+++ a.c
@@ -29,19 +29,26 @@ int my_select(OXFILE *server) {
 void reset(OXFILE *server) { ox_reset(server); }
 void shutdown(OXFILE *server) { ox_shutdown(server); }
 
-int main(void) {
+#include <unistd.h>
+int main(void) { // test
 
   OXFILE *server = start();
   execute_string(server, "1000000000 + 98765;");
+  puts("sleep 1 sec ...");
+  sleep(1);
   assert(my_select(server) != 0);
   char *res1 = pop_string(server);
   assert(strcmp(res1, "1000098765") == 0);
 
   execute_string(server, "fctr(x^200000 - y^200000);");
+  puts("sleep 1 sec ...");
+  sleep(1);
   assert(my_select(server) == 0); // 計算中
   reset(server);
 
   execute_string(server, "fctr(x^2 - y^2);");
+  puts("sleep 1 sec ...");
+  sleep(1);
   assert(my_select(server) != 0);
   char *res2 = pop_string(server);
   assert(strcmp(res2, "[[1,1],[x-y,1],[x+y,1]]") == 0);
--- b.c
+++ b.c
@@ -11,19 +11,26 @@ extern int my_select(OXFILE *);
 extern void reset(OXFILE *);
 extern void shutdown(OXFILE *);
 
-int main(void) {
+#include <unistd.h>
+int main(void) { // test
 
   OXFILE *server = start();
   execute_string(server, "1000000000 + 98765;");
+  puts("sleep 1 sec...");
+  sleep(1);
   assert(my_select(server) != 0);
   char *res1 = pop_string(server);
   assert(strcmp(res1, "1000098765") == 0);
 
   execute_string(server, "fctr(x^200000 - y^200000);");
+  puts("sleep 1 sec...");
+  sleep(1);
   assert(my_select(server) == 0); // 計算中
   reset(server);
 
   execute_string(server, "fctr(x^2 - y^2);");
+  puts("sleep 1 sec...");
+  sleep(1);
   assert(my_select(server) != 0);
   char *res2 = pop_string(server);
   assert(strcmp(res2, "[[1,1],[x-y,1],[x+y,1]]") == 0);
